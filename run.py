241a3ba5d9d2168de31bb0a25b1e8ff3871dc56b
unknown <hchamrouk.pro@gmail.com>
2024-11-25 13:11:11 +0100

    add xml sitemap

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from portfolio.app import portfolio_app
 from QuranTraining.app import quran_app
 import logging
 from flask_babel import Babel, _
+from flask_sitemap import Sitemap
 
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
@@ -11,13 +12,22 @@ app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'fr'] 
 babel = Babel(app)
 
-def get_locale():
-    return request.accept_languages.best_match(['en', 'fr'])
-
-babel.init_app(app, locale_selector=get_locale)
+app.config['SITEMAP_INCLUDE_RULES_WITHOUT_PARAMS'] = True
+sitemap_ext = Sitemap(app)
 
 app.register_blueprint(portfolio_app, url_prefix='/')
 app.register_blueprint(quran_app, url_prefix='/QuranTraining')
+
+def get_locale():
+    return request.accept_languages.best_match(['en', 'fr'])
+
+@app.route('/sitemap.xml', methods=['GET'])
+def generate_sitemap():
+    sitemap_xml = sitemap_ext.generate()
+    response = Response(sitemap_xml, content_type='application/xml')
+    return response
+
+babel.init_app(app, locale_selector=get_locale)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=False)
