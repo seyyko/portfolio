08789b6a1c2a891e009ea181066d0dceaac51c6f
unknown <hchamrouk.pro@gmail.com>
2024-11-29 04:13:28 +0100

    update the translation

--- static/QuranTraining/js/compressionWorker.js
+++ static/QuranTraining/js/compressionWorker.js
@@ -5,11 +5,9 @@ self.onmessage = function (e) {
     const { action, key, data } = e.data;
 
     if (action === 'compress') {
-        // Compresse les données et retourne le résultat
         const compressedData = LZString.compress(JSON.stringify(data));
         self.postMessage({ key, result: compressedData });
     } else if (action === 'decompress') {
-        // Décompresse les données et retourne le résultat
         const decompressedData = JSON.parse(LZString.decompress(data) || 'null');
         self.postMessage({ key, result: decompressedData });
     } else {
